ae18e0eab1ac27a18ed0ffe9b4282ec8a21e755f
GiYeong Um <eomky2005@gmail.com>
2022-06-12 15:46:07 +0900

    [update] initial commit

---
+++ src/main/kotlin/com/gyu/spring_test/SpringTestProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.gyu.spring_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringTestProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringTestProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/gyu/spring_test/SpringTestProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gyu.spring_test
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTestProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
